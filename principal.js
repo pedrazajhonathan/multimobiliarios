3db8ca24ee62b830565fbe2de09c128af00caac3
jhonathanpedraza <pedrazajhonathan9@Gmail.com>
2026-03-10 12:21:13 -0400

    colocando el link de compra

--- principal.js
+++ principal.js
@@ -216,7 +216,7 @@ function alejir(i){
         let contenido = document.createElement("div")
         contenido.classList.add("contenido")
 
-        enlace.href = ""
+        enlace.href = "https://wa.me/584120556265"
         imagen_close.src = img_close
         precio.textContent = dat.precio
         enlace.appendChild(imagen_close)
